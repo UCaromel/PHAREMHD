f9ade4644d7e8c9faedfefde8200d9c8b9bf4b62
UCaromel <caromelulysse@gmail.com>
2024-04-25 14:57:57 +0200

    README update

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - Make the code more modular
 
 # And then
-- Extend the model (3D, Energy Equation instead of hard coded P, Non-Ideal MHD, more sofisticated Riemann solvers, more sophisticated CT).
+- Extend the model (3D, Non-Ideal MHD, more sofisticated Riemann solvers, more sophisticated CT ...).
 - Clean up the code.
 - Integrate in PHARE.
 - Add coupling (Fluid-Fluid, Fluid-Hybrid)
--- src/ComputeNewDt.hpp
+++ src/ComputeNewDt.hpp
@@ -20,7 +20,7 @@ double ComPuteNewDt(const ConservativeVariablesCC& C_cc, double Dx, double Dy, i
             double cay = std::sqrt((C_cc(i,j).By*C_cc(i,j).By)/(4*M_PI*C_cc(i,j).rho)); // Alfven speeds in y
             double ca = std::sqrt((C_cc(i,j).Bx*C_cc(i,j).Bx + C_cc(i,j).By*C_cc(i,j).By + C_cc(i,j).Bz*C_cc(i,j).Bz)/(4*M_PI*C_cc(i,j).rho)); // Alfven speeds
             double cfastx = std::sqrt((c0*c0 + ca*ca)*0.5 + (std::sqrt((c0*c0 + ca*ca)*(c0*c0 + ca*ca) - 4*c0*c0*cax*cax))*0.5); // Fast magnetosonic speeds in x
-            double cfasty = std::sqrt((c0*c0 + ca*ca)*0.5 + (std::sqrt((c0*c0 + ca*ca)*(c0*c0 + ca*ca) - 4*c0*c0*cay*cay))*0.5); // Fast magnetosonic speeds in x
+            double cfasty = std::sqrt((c0*c0 + ca*ca)*0.5 + (std::sqrt((c0*c0 + ca*ca)*(c0*c0 + ca*ca) - 4*c0*c0*cay*cay))*0.5); // Fast magnetosonic speeds in y
             sum.push_back(cfastx/Dx + cfasty/Dy);
         }
     }
